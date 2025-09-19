2b21063c8969ec4e8013d5164cf02af4c7a7ef11
Neyo <naledihloni14@gmail.com>
2025-09-19 17:50:54 +0200

    Update basic_operations.py

--- basic_operations.py
+++ basic_operations.py
@@ -5,6 +5,6 @@ add = number1 + number2
 sub = number1 - number2
 multi = number1 * number2
 
-print(add)
-print(sub)
-print(multi)
+print('Addition of 10 and 5 is',add)
+print('Subtraction of 10 and 5 is',sub)
+print('Multiplication of 10 and 5 is',multi)
