ec3926be24634658a3dca453d1b917395bea05ce
Neyo <naledihloni14@gmail.com>
2025-09-23 00:35:40 +0200

    Add files via upload

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,10 +1,10 @@
-season = input('What is the weather like today? (sunny/rainy/cold): ')
+season = input('What\'s the weather like today? (sunny/rainy/cold): ')
 
 if season == 'sunny':
     print('Wear a tshirt and sunglasses.')
 elif season == 'rainy':
     print('Don\'t forget your umbrella and raincoat.')
-elif seaason == 'cold':
+elif season == 'cold':
     print('Make sure to wear a warm jacket and gloves.') 
 else:
     print('Sorry, I don\'t have recommendations for that eather choice.')
