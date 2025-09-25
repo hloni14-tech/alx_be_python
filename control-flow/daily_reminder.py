296ef0cbfd4f179664b84f58f2290c99a0b2c744
Neyo <naledihloni14@gmail.com>
2025-09-26 00:39:30 +0200

    Delete control-flow/daily_reminder.py

--- control-flow/daily_reminder.py
+++
@@ -1,8 +0,0 @@
-question = input('Enter your task: ')
-priority = input('high/medium/low: ').lower()
-time_bound = input('Is it time bound? (yes/no): ').lower()
-
-if time_bound == 'yes':
-    print('Reminder:',question,'is a high priority task that requires immediate attention today!' )
-elif time_bound == 'no':
-    print('Note:',question,'is a low priority task.Consider completing it when you have free time.')
