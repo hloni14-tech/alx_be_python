328103e5afecd164416c803c3dd218a109b95557
Neyo <naledihloni14@gmail.com>
2025-09-26 00:40:28 +0200

    Add files via upload

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,8 @@
+task = input('Enter your task: ')
+priority = input('high/medium/low: ')
+time_bound = input('Is it time bound? (yes/no): ')
+
+if time_bound == 'yes'and  priority == 'high':
+    print('Reminder:',question,'is a high priority task that requires immediate attention today!' )
+elif time_bound == 'no' and priority == 'low':
+    print('Note:',question,'is a low priority task.Consider completing it when you have free time.')
