1cfb68064d0352f6eccfd5ac502b072f99ead298
Neyo <naledihloni14@gmail.com>
2025-09-26 00:35:41 +0200

    Add files via upload

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,23 @@
+num1 = (input('Enter the first number: '))
+num2 = (input('Enter the second number: '))
+operation = (input('Choose the operation(+,-,*,/): '))
+
+
+if operation == '+':
+ sum = float(num1) + float(num2)
+ print('The result is:',sum)
+elif operation == '-':
+ sum = float(num1) - float(num2)
+ print('The result is:',sum)
+elif operation == '*':
+ sum  = float(num1) * float(num2)
+ print('The result is:',sum)
+elif num1 == 0 or operation == '/':
+ print('Cannot divide by zero')
+elif num2 == 0 or operation == '/':
+    print('Cannot divide by zero')
+elif operation == '/':
+ sum = float(num1) / float(num2)
+ print('The result is:',sum)
+
+
