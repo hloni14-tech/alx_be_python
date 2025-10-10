b3407ec87abba251f67cdae7512e1e183b829d32
Neyo <naledihloni14@gmail.com>
2025-10-10 19:38:52 +0200

    Add files via upload

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,22 @@
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.__account_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            return True
+        return False
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.__account_balance:
+            self.__account_balance -= amount
+            return True
+        return False
+
+    def display_balance(self):
+        print("Current Balance: $",self.__account_balance,00)
+
+    def get_balance(self):
+        return self.__account_balance
+    
