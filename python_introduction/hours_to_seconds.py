d48b3c0cbdc7c7208907200f658f8a2fba910fce
Neyo <naledihloni14@gmail.com>
2025-11-18 11:28:54 +0200

    Update hours_to_seconds.py

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,4 +1,9 @@
+seconds = hours * 3600 
+seconds=hours*3600 
+seconds = 3600 * hours 
+seconds=3600*hours
 hours = 2
 seconds = 60 * 60 == 3600
 time = 2 * 3600
 print('2 hour(s) is',time, 'seconds(s))')
+
